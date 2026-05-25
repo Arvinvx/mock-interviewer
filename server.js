9ab4241474b85a4b6bfbf5a5302365b62b5b7e81
Arvin Dehghan <arvindisocrd3@gmail.com>
2026-05-25 23:14:43 +0300

    serve frontend static files

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 import express from 'express'
 import cors from 'cors'
 import router from "./routes/router.js"
+import path from 'path'
 import 'dotenv/config'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 
-
+app.use(express.static(path.join(__dirname, 'frontend')))
 app.use(cors())
 app.use(express.json())
 
